538463e55587eb194bafc7207340fe3dcd97850f
Subiksha S <arunasaravanan41@gmail.com>
2024-09-17 08:55:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+export default App
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Loader } from "lucide-react";
 
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="h-screen w-screen flex justify-center items-center bg-neutral-950">
+        <div className="h-3/6 w-[30%] flex justify-center items-center bg-white rounded-md shadow-2xl">
+          <form className="w-[80%] h-[80%] flex flex-col justify-center items-center gap-4">
+            <input type="text" name="" id="" placeholder="Name" className="p-3 bg-[#f1d731] w-full font-bold outline-none active:outline-none focus:border-b-2 hover:border-red-300" />
+            <input type="text" name="" id="" placeholder="Email" className="p-3 bg-[#f1d731] w-full font-bold outline-none active:outline-none focus:border-b-2 hover:border-red-300" />
+            <input type="number" name="" id="" placeholder="Phone" className="p-3 bg-[#f1d731] w-full font-bold outline-none active:outline-none focus:border-b-2 hover:border-red-300" />
+            <input type="password" name="" id="" placeholder="Password" className="p-3 bg-[#f1d731] w-full font-bold outline-none active:outline-none focus:border-b-2 hover:border-red-300" />
+            <button type="submit" className="w-full bg-cyan-400 text-black p-3"> <Loader  className="animate-spin"/>  Register</button>
+          </form>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
-
-export default App
+export default App;
